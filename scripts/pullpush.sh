68e46bbdd5ab5f5a48df792bd019a37fb8d86733
yylt <yang8518296@163.com>
2021-11-03 16:45:02 +0800

    add more test image

--- scripts/pullpush.sh
+++ scripts/pullpush.sh
@@ -42,3 +42,25 @@ while IFS= read -r line; do
         fi
     fi   
 done < $fpath
+
+while IFS= read -r line; do
+    registry=$(echo -n  $line |cut -d / -f 1)
+    name=${line##*/}
+    newrepo="docker.io/yylt/$name"
+
+    if [ $registry = 'docker.io' ];then
+       continue
+    else
+        if [ $(exist $newrepo) -eq 0 ];then
+            continue
+        fi    
+        docker pull --platform linux/amd64 $line
+        if [ $? -eq 0 ] ;then
+            docker tag "$line" $newrepo
+            docker push $newrepo
+            if [ $? -ne 0 ] ;then
+                exit -1
+            fi
+        fi
+    fi   
+done < $fpath
