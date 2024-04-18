e8bfd803c323e99f794b245983f717b9f4a68c7f
yylt <yang8518296@163.com>
2024-04-18 16:10:37 +0800

    kata

--- kata-image/kata-image.mk
+++ kata-image/kata-image.mk
@@ -9,35 +9,68 @@
 		docker-push \
 		agent \
 		all \
+		package \
 		clean
 
-CONTAINERD_SHIM_NAME = "containerd-shim-ecr-v2"
-ECR_RUNTIME = "ecr-runtime"
-CONFIG_FILE_NAME = "configuration.toml"
-SOURCE_DIR = "./src/runtime"
-AGENT_DIR = "./src/agent"
-REGISTRY_NAME = "registry.yylt.tk:41072"
-IMAGE_NAME = $(REGISTRY_NAME)/captain/ecr-deploy
-LAST_COMMIT_ID = $(shell git rev-parse HEAD)
-IMAGE_TAG = $(shell arch)-$(LAST_COMMIT_ID)
+CONTAINERD_SHIM_NAME ?= containerd-shim-ecr-v2
+ECR_RUNTIME ?= ecr-runtime
+CONFIG_FILE_NAME ?= configuration.toml
+SOURCE_DIR ?= ../src/runtime
+AGENT_DIR ?= ../src/agent
+REGISTRY_NAME ?= ghcr.io
+IMAGE_NAME ?= $(REGISTRY_NAME)/yylt/ecr-deploy
 
-all: generate-config containerd-shim-v2 ecr-runtime build-kernel build-image docker-push
+ifeq ($(shell arch),aarch64)
+	# aarch64 环境重新命名 IMAGE_NAME 结构
+	IMAGE_NAME = $(REGISTRY_NAME)/arm64v8/ecr-deploy
+endif
+
+LAST_COMMIT_ID = $(shell git rev-parse HEAD)
+IMAGE_TAG = v$(shell cat ../VERSION)-$(LAST_COMMIT_ID)
+
+
+all: generate-config build-image build-kernel ecr-runtime containerd-shim-v2 docker-push
 
 containerd-shim-v2:
-	./ci/install_yq.sh
+ifeq ($(shell arch),x86_64)
 	make -C $(SOURCE_DIR) containerd-shim-v2
 	mv $(SOURCE_DIR)/containerd-shim-kata-v2 $(CONTAINERD_SHIM_NAME)
+endif
 
+
+ifeq ($(shell arch),aarch64)
+	# aarch64 如果在新建的容器中运行，需要安装如下组件：
+	# apt-get install -y curl wget sudo gcc
+
+	../ci/install_yq.sh; \
+	ln -sf $$GOPATH/bin/yq /usr/bin/yq; \
+	mkdir -p /opt/go $$HOME/go; \
+	export GOROOT="/opt/go"; \
+	export PATH=$$PATH:"/opt/go/bin":"$$HOME/go/bin"; \
+	../ci/install_go.sh; \
+	make -C $(SOURCE_DIR) containerd-shim-v2; \
+	mv $(SOURCE_DIR)/containerd-shim-kata-v2 $(CONTAINERD_SHIM_NAME)
+endif
 
 ecr-runtime:
+ifeq ($(shell arch),x86_64)
 	make -C $(SOURCE_DIR) runtime
 	mv $(SOURCE_DIR)/kata-runtime $(ECR_RUNTIME)
+endif
+
+ifeq ($(shell arch),aarch64)
+	mkdir -p /opt/go $$HOME/go; \
+	export GOROOT="/opt/go"; \
+	export PATH=$$PATH:"/opt/go/bin":"$$HOME/go/bin"; \
+	make -C $(SOURCE_DIR) runtime
+	mv $(SOURCE_DIR)/kata-runtime $(ECR_RUNTIME)
+endif
 
 # 根据架构不同生成安全容器配置文件
 generate-config:
 ifeq ($(shell arch),x86_64)
 	# make -C $(SOURCE_DIR) clean
-	SKIP_GO_VERSION_CHECK=y make -C $(SOURCE_DIR) cli/config/configuration-qemu.toml \
+	SKIP_GO_VERSION_CHECK=y make -C $(SOURCE_DIR) config/configuration-qemu.toml \
 	QEMUPATH="/usr/libexec/qemu-kvm" \
 	KERNELPATH="/usr/share/ecr/vmlinuz.container" \
 	IMAGEPATH="/usr/share/ecr/ecr-containers.img" \
@@ -46,27 +79,31 @@ ifeq ($(shell arch),x86_64)
 	DEFMEMSZ="2048" \
 	DEFVIRTIOFSDAEMON="/usr/libexec/virtiofsd" \
 	DEFVALIDVIRTIOFSDAEMONPATHS='[\"/usr/libexec/virtiofsd\"]' \
-	DEFSANDBOXCGROUPONLY="true"
-	mv $(SOURCE_DIR)/cli/config/configuration-qemu.toml configuration.toml
+	DEFSANDBOXCGROUPONLY="true" \
+	DEFENABLEANNOTATIONS='[\"default_vcpus\",\"guest_hook_path\",\"kernel\",\"image\",\"enable_sriov\",\"scsi_scan_mod\",\"hotplug_vfio_on_root_bus\",\"pcie_root_port\",\"sandbox_cgroup_only\",\"read_only_rootfs\"]'
+	mv $(SOURCE_DIR)/config/configuration-qemu.toml configuration.toml
 endif
 
 ifeq ($(shell arch),aarch64)
 	# make -C $(SOURCE_DIR) clean
-	SKIP_GO_VERSION_CHECK=y make -C $(SOURCE_DIR) cli/config/configuration-qemu.toml \
+	SKIP_GO_VERSION_CHECK=y make -C $(SOURCE_DIR) config/configuration-qemu.toml \
 	QEMUPATH="/usr/libexec/qemu-kvm" \
 	KERNELPATH="/usr/share/ecr/vmlinuz.container" \
 	IMAGEPATH="/usr/share/ecr/ecr-containers.img" \
 	QEMUVALIDHYPERVISORPATHS='[\"/usr/libexec/qemu-kvm\"]' \
-	CPUFEATURES="pmu=off,vmx=off" \
+	CPUFEATURES="pmu=off" \
 	DEFMEMSZ="2048" \
 	DEFVIRTIOFSDAEMON="/usr/libexec/virtiofsd" \
 	DEFVALIDVIRTIOFSDAEMONPATHS='[\"/usr/libexec/virtiofsd\"]' \
-	DEFSANDBOXCGROUPONLY="true"
-	mv $(SOURCE_DIR)/cli/config/configuration-qemu.toml configuration.toml
+	DEFSANDBOXCGROUPONLY="true" \
+	DEFAULTPFLASHES='[\"/usr/share/ecr/ecr-flash0.img\", \"/usr/share/ecr/ecr-flash1.img\"]' \
+	DEFAULTDISABLEIMAGENVDIMM="true" \
+	DEFENABLEANNOTATIONS='[\"default_vcpus\",\"kernel\",\"image\",\"scsi_scan_mod\",\"hotplug_vfio_on_root_bus\",\"pcie_root_port\",\"sandbox_cgroup_only\",\"read_only_rootfs\"]'
+	mv $(SOURCE_DIR)/config/configuration-qemu.toml configuration.toml
 endif
 
-build-kernel:
-	sudo -E apt-get install -y \
+package:
+	sudo -E apt install -y \
 		gcc \
 		make \
 		libncurses5-dev \
@@ -78,67 +115,98 @@ build-kernel:
 		bison \
 		flex \
 		libelf-dev \
+		libfl-dev \
+		libseccomp-dev \
 		bc
+
+build-kernel: package
+
 ifeq ($(shell arch),x86_64)
-	cd ./tools/packaging/kernel; \
-	sudo -E ./build-kernel.sh -v 5.10.25 -g nvidia -f -d setup; \
-	sudo -E ./build-kernel.sh -v 5.10.25 -g nvidia -f -d build; \
-	sudo -E ./build-kernel.sh -v 5.10.25 -g nvidia -f -d install; \
-	cd ../../..; \
-	sudo -E cp /usr/share/kata-containers/vmlinuz-nvidia-gpu.container ./ecr_deploy/vmlinuz.container
+	cd ../tools/packaging/kernel; \
+	sudo -E ./build-kernel.sh -v 5.15.63 -g nvidia -f -d setup; \
+	sudo -E ./build-kernel.sh -v 5.15.63 -g nvidia -f -d build; \
+	sudo -E ./build-kernel.sh -v 5.15.63 -g nvidia -f -d install; \
+	sudo -E cp /usr/share/kata-containers/vmlinux-5.15.63-96-nvidia-gpu ../../../ecr_deploy/vmlinuz.container
 endif
 
 ifeq ($(shell arch),aarch64)
-	cd ./tools/packaging/kernel; \
-	sudo -E ./build-kernel.sh -v 5.10.25 -f -d setup; \
-	sudo -E ./build-kernel.sh -v 5.10.25 -f -d build; \
-	sudo -E ./build-kernel.sh -v 5.10.25 -f -d install; \
-	cd ../../..; \
-	sudo -E cp /usr/share/kata-containers/vmlinuz.container ./ecr_deploy/vmlinuz.container
+	cd ../tools/packaging/kernel; \
+	sudo -E ./build-kernel.sh -v 5.15.63 -f -d setup -a aarch64 -E arch-experimental; \
+	sudo -E ./build-kernel.sh -v 5.15.63 -f -d build -a aarch64 -E arch-experimental; \
+	sudo -E ./build-kernel.sh -v 5.15.63 -f -d install -a aarch64 -E arch-experimental; \
+	sudo -E cp /usr/share/kata-containers/vmlinuz.container ../../../ecr_deploy/vmlinuz.container
 endif
 
 
 build-image: agent
-	cp ./src/agent/target/*/release/kata-agent /tmp/; \
-	export IMAGE_REGISTRY="docker.io --network host"; \
-	export AGENT_SOURCE_BIN="/tmp/kata-agent"; \
+ifeq ($(shell arch),x86_64)
+	sudo -E apt install -y qemu-utils multistrap; \
+	cd ../tools/osbuilder; \
 	export USE_DOCKER=true; \
-	export DEBUG=true; \
-	cd ./tools/osbuilder/rootfs-builder; \
-	sudo -E ./rootfs.sh centos; \
+	export EXTRA_PKGS="chrony coreutils gcc make curl gnupg  apt tar kmod pkg-config libc-dev libc6-dev pciutils bridge-utils iproute2 iputils-ping iputils-arping"; \
+	export ROOTFS_DIR=$${GOPATH}/src/github.com/kata-containers/kata-containers/tools/osbuilder/rootfs-builder/rootfs; \
+	export AGENT_SOURCE_BIN="$${GOPATH}/src/github.com/kata-containers/kata-containers/src/agent/target/x86_64-unknown-linux-musl/release/kata-agent"; \
+	cd rootfs-builder; \
+	sudo -E ./rootfs.sh ubuntu; \
 	cd ..; \
-	sudo -E cp ../../src/agent/kata-agent.service rootfs-builder/rootfs-Centos/usr/lib/systemd/system/; \
-	sudo -E cp ../../src/agent/kata-containers.target rootfs-builder/rootfs-Centos/usr/lib/systemd/system/; \
-	sudo -E ./image-builder/image_builder.sh rootfs-builder/rootfs-Centos; \
+	sudo -E cp ../../src/agent/kata-agent.service rootfs-builder/rootfs/etc/systemd/system/; \
+	sudo -E cp ../../src/agent/kata-containers.target rootfs-builder/rootfs/etc/systemd/system/; \
+	sudo -E ./image-builder/image_builder.sh rootfs-builder/rootfs; \
 	sudo -E cp kata-containers.img ../../ecr_deploy/ecr-containers.img
-
-
-agent:
-ifeq ($(shell arch),aarch64)
-	./ci/install_musl.sh
 endif
-	make -C $(AGENT_DIR) -e PATH=$$PATH:/usr/local/musl/bin kata-agent
-	make -C $(AGENT_DIR) -e PATH=$$PATH:/usr/local/musl/bin kata-agent.service
 
+ifeq ($(shell arch),aarch64) 
+	sudo -E apt install -y qemu-utils multistrap; \
+	cd ../tools/osbuilder; \
+	export EXTRA_PKGS="chrony coreutils gcc make curl gnupg  apt tar kmod pkg-config libc-dev libc6-dev pciutils bridge-utils iproute2 iputils-ping iputils-arping"; \
+	export ROOTFS_DIR=$${GOPATH}/src/github.com/kata-containers/kata-containers/tools/osbuilder/rootfs-builder/rootfs; \
+	export AGENT_SOURCE_BIN="$${GOPATH}/src/github.com/kata-containers/kata-containers/src/agent/target/aarch64-unknown-linux-musl/release/kata-agent"; \
+	cd rootfs-builder; \
+	sudo -E ./rootfs.sh ubuntu; \
+	cd ..; \
+	sudo -E cp ../../src/agent/kata-agent.service rootfs-builder/rootfs/etc/systemd/system/; \
+	sudo -E cp ../../src/agent/kata-containers.target rootfs-builder/rootfs/etc/systemd/system/; \
+	sudo -E ./image-builder/image_builder.sh rootfs-builder/rootfs; \
+	sudo -E cp kata-containers.img ../../ecr_deploy/ecr-containers.img
+endif
+
+
+agent: package
+ifeq ($(shell arch),x86_64)
+	# make -C $(AGENT_DIR) clean
+	export LIBC = gnu
+	make -C $(AGENT_DIR) kata-agent
+	make -C $(AGENT_DIR) kata-agent.service
+endif
+
+ifeq ($(shell arch),aarch64)
+	# 注意：需要提前 clone kata-containers/tests 项目
+	../ci/install_musl.sh; \
+	../ci/install_rust.sh; \
+	export PATH=$$PATH:"$$HOME/.cargo/bin"; \
+	rustup target add aarch64-unknown-linux-gnu; \
+	export LIBC = gnu; \
+	rustup component add rustfmt clippy; \
+	make -C $(AGENT_DIR) SECCOMP=no kata-agent; \
+	make -C $(AGENT_DIR) kata-agent.service
+endif
 
 docker-image:
 ifeq ($(shell arch),x86_64)
-	IMAGE_NAME=$(REGISTRY_NAME)/captain/ecr-deploy
+	docker build -t $(IMAGE_NAME):$(IMAGE_TAG) -f Dockerfile .
 endif
 ifeq ($(shell arch),aarch64)
-	IMAGE_NAME=$(REGISTRY_NAME)/arm64v8/ecr-deploy
-endif	
-	docker build -t $(IMAGE_NAME):$(IMAGE_TAG) -f ./ecr_deploy/Dockerfile ./ecr_deploy
+	docker build -t $(IMAGE_NAME):$(IMAGE_TAG) -f Dockerfile .
+	# docker buildx build --platform=linux/arm64 -t $(IMAGE_NAME):$(IMAGE_TAG) -f Dockerfile .
+endif
 
 docker-push: docker-image
 ifeq ($(shell arch),x86_64)
-	IMAGE_NAME=$(REGISTRY_NAME)/captain/ecr-deploy
+	docker push $(IMAGE_NAME):$(IMAGE_TAG)
 endif
 ifeq ($(shell arch),aarch64)
-	IMAGE_NAME=$(REGISTRY_NAME)/arm64v8/ecr-deploy
-endif	
 	docker push $(IMAGE_NAME):$(IMAGE_TAG)
-
+endif
 
 clean:
 	rm -f $(CONTAINERD_SHIM_NAME) $(ECR_RUNTIME) $(CONFIG_FILE_NAME) ecr-containers.img vmlinuz.container
