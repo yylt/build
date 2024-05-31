a7796abaa93cb60aba6969bca333e6844a9565fe
yylt <yang8518296@163.com>
2024-05-31 10:13:31 +0800

    cni

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,12 +19,16 @@ INSTALL_CONFIG=${INSTALL_CONFIG:-"true"}
 SLEEP_LOOP=${SLEEP_LOOP:-"true"}
 INSTALL_CNI=${INSTALL_CNI:-"true"}
 WHEREABOUTS_LOOP=${WHEREABOUTS_LOOP:-"true"}
-# Make a whereabouts.d directory (for our kubeconfig)
+INSTALL_CNI_CONFLIST=${INSTALL_CNI_CONFLIST:-"false"}
 
+# Make a whereabouts.d directory (for our kubeconfig)
 mkdir -p $CNI_CONF_DIR/whereabouts.d
+
 WHEREABOUTS_KUBECONFIG=$CNI_CONF_DIR/whereabouts.d/whereabouts.kubeconfig
 WHEREABOUTS_FLATFILE=$CNI_CONF_DIR/whereabouts.d/whereabouts.conf
 WHEREABOUTS_KUBECONFIG_LITERAL=$(echo "$WHEREABOUTS_KUBECONFIG" | sed -e s'|/host||')
+
+INSTALL_CNI_CONFLISTPATH=$CNI_CONF_DIR/99-simple.conflist
 
 # ------------------------------- Generate a "kube-config"
 SERVICE_ACCOUNT_PATH=/var/run/secrets/kubernetes.io/serviceaccount
@@ -48,7 +52,7 @@ function warn()
     log "WARN: {$1}"
 }
 
-function installconfig()
+function installwhereaboutsconfig()
 {
     if [ "$INSTALL_CONFIG" != "true"  ]; then
         warn "not install whereabouts config"
@@ -126,10 +130,53 @@ fi
 function installcni()
 {
     if [ "$INSTALL_CNI" == "true"  ]; then
-        log "install cni plugins to dir: $CNI_BIN_DIR"
-        find /cni-plugins*tgz -exec tar xvfpz {} -C $CNI_BIN_DIR/ \;
+        log "install cni plugins"
+        find /plugins -type f -perm -111 -exec cp {} $CNI_BIN_DIR/ \;
     fi 
 }
+
+function installcniconf()
+{
+  if [ "$INSTALL_CNI_CONFLIST" == "true"  ]; then
+  cat > $INSTALL_CNI_CONFLISTPATH <<-'EOF'
+{
+  "cniVersion": "0.4.0",
+  "name": "simple",
+  "plugins": [
+    {
+      "type": "bridge",
+      "bridge": "cni-simple",
+      "isGateway": true,
+      "ipMasq": true,
+      "hairpinMode": true,
+      "ipam": {
+        "type": "host-local",
+        "routes": [{ "dst": "0.0.0.0/0" }],
+        "ranges": [
+          [
+            {
+              "subnet": "10.42.0.0/16",
+              "gateway": "10.42.0.1"
+            }
+          ]
+        ]
+      }
+    },
+    {
+      "type": "portmap",
+      "capabilities": {
+        "portMappings": true
+      }
+    }
+  ]
+}
+
+EOF
+
+        
+  fi 
+}
+
 
 function whereaboutsloop()
 {
@@ -142,12 +189,15 @@ function whereaboutsloop()
 function sleeploop()
 {
     while [ "$SLEEP_LOOP" == "true"  ]; do
-        log "Done configuring CNI.  Sleep=$should_sleep"
-        sleep 1000000000000
+      log "Entering sleep... (success)"
+      # Sleep forever. 
+      # sleep infinity is not available in alpine; instead lets go sleep for ~68 years. Hopefully that's enough sleep
+      sleep 2147483647
     done
 }
 
 installcni
-installconfig
+installcniconf
+installwhereaboutsconfig
 whereaboutsloop
 sleeploop
